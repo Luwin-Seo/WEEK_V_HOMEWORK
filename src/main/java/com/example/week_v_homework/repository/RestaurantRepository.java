8ee7ab91492214a62bc4e20e21b1510fb755b9a4
Luwin-Seo <aksdnjs88@gmail.com>
2022-06-08 21:28:05 +0900

    공유를 위한 푸시

--- src/main/java/com/example/week_v_homework/repository/RestaurantRepository.java
+++ src/main/java/com/example/week_v_homework/repository/RestaurantRepository.java
@@ -3,5 +3,8 @@ package com.example.week_v_homework.repository;
 import com.example.week_v_homework.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    Optional<Restaurant> findByName(String name);
 }
